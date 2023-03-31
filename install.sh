e9a33146c3ebe6464d3b3f9757aae02b6dbd1b96
woaishuijiao <1438128842@qq.com>
2023-03-31 08:59:39 -0700

    add install bash

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
-#!/bin/sh
-# git curl
+#!/bin/bash
+files="vimrc"
 
-# 安装CtrlP
-mkdir -p ~/.vim/pack/vendor/start
-cd ~/.vim/pack/vendor/start
-git clone https://github.com/ctrlpvim/ctrlp.vim
-
-#安装vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+for file in $files; do
+	ln -s ~/dotfiles/$file ~/.$file
+done
